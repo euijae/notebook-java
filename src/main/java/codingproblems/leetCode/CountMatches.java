040b1b116d94bb79a9ac39bbfea4630e31a8d3ff
Euijae Kim <acreclan49@gmail.com>
2021-03-02 19:55:34 -0800

    Create CountMatches.java
    init

---
+++ src/main/java/codingproblems/leetCode/CountMatches.java
@@ -0,0 +1,27 @@
+/**
+ *  Input: items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]],  
+ *          ruleKey = "color", 
+ *          ruleValue = "silver"
+ *  Output: 1
+ *  Explanation: There is only one item matching the given rule, which is ["computer","silver","lenovo"].
+ */
+class CountMatches {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int counter = 0;
+        Map<String, Integer> map = new HashMap<>();
+        map.put("type", 0);
+        map.put("color", 1);
+        map.put("name", 2);
+        
+        for(int i = 0; i < items.size(); i++) {
+            List<String> item = items.get(i);
+            
+            if(map.containsKey(ruleKey)) {
+                int index = map.get(ruleKey);
+                counter += (item.get(index).equals(ruleValue) ? 1: 0);
+            } 
+        }
+        
+        return counter;
+    }
+}
